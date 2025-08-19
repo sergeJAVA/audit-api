fb36004d8bd85be4476315ae51079883d84c5855
Sergey Kurochkin <serega.curo4kin2014@gmail.com>
2025-08-19 12:15:38 +0300

    first commit

---
+++ src/main/java/com/webbee/audit_api/AuditApiApplication.java
@@ -0,0 +1,13 @@
+package com.webbee.audit_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webbee/audit_api/AuditApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webbee.audit_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
